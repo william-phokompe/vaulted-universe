58ae734f860240a67b8afd064d5a2ea39374d1ad
William Phokompe <phokompe.william@outlook.com>
2019-09-10 12:34:10 +0200

    Create exercise
    Finding one record by userId and push the exercise onto the exercise field, then save the document

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var users = function(request, response) {
 }, createUser = function(request, response) {
     var new_user = new User({
         userId: id.generate(),
-        username: request.body.username
+        username: request.body.username,
+        exercise: request.body.exercise
     });
 
     new_user.save((error, result) => {
@@ -20,10 +21,30 @@ var users = function(request, response) {
             response.send(error);
         response.json(result);
     });
-};
+}, createExercise = function(request, response) {
+    var newExercise = new Exercise(
+        request.body.userid,
+        request.body.description,
+        request.body.duration,
+        new Date(request.body.date)
+    );
 
+    User.findOne({ userId: newExercise.userId }, (error, data) => {
+        if (error)
+            response.send(error);
+        var document = new User(data);
+        document.exercise.push(newExercise);
+        document.save((err, res) => {
+            if (err) {
+                response.send(err);
+            }
+            response.json(res);
+        });
+    });
+};
 
 module.exports = {
     users,
-    createUser
+    createUser,
+    createExercise
 };
