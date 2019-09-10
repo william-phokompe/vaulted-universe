62392704b047e1874c686bbf03c4784dddc46a10
William Phokompe <phokompe.william@outlook.com>
2019-09-10 11:16:31 +0200

    CreateUser and GetAllUsers endpoints

--- control/routes.js
+++ control/routes.js
@@ -1,3 +1,10 @@
+
 module.exports = function(app) {
-    
+    var logic = require('../app');
+
+    app.route('/')
+        .get(logic.users);
+
+    app.route('/createUser')
+        .post(logic.createUser);
 }
