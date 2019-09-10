2718badb5fcfc1324b28c8025b7d1498804f0c0a
William Phokompe <phokompe.william@outlook.com>
2019-09-10 10:42:55 +0200

    import all necessary modules, create a DB Connection and open server

--- server.js
+++ server.js
@@ -3,3 +3,32 @@ if (process.env.NODE_ENV !== 'production') {
 }
 require('./models/exercise');
 require('./models/user');
+var mongoose = require('mongoose'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    routes = require('./control/routes'),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+    `${process.env.EXERCISE_URI.toString()}&w=majority`,
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+).then(_ => {
+    app.listen(port);
+}).then(_ => {
+    console.log(`All is goo at: ${port}`);
+}).catch((err) => {
+    console.log(`What the heck? ${err}`);
+});
+
+routes(app);
