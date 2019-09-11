e2236b4306077fdb82eefff87be3cd1fff8e9c0a
William Phokompe <phokompe.william@outlook.com>
2019-09-11 14:45:55 +0200

    Logic behind the logs route
    /api/exercise/log?{userId}&[from]&[to]&[limit]

--- app.js
+++ app.js
@@ -49,11 +49,56 @@ var drawFields = function(_, response) {
             response.json(res);
         });
     });
+}, findUser = function(request, response) {
+    var filter = { userId: request.query.userId };
+    User.find(filter)
+        .where('exercise')
+        .exec((error, resp) => {
+            if (error) {
+                response.send(error);
+                return;
+            }
+            var obj = JSON.parse(JSON.stringify(resp[0]));
+            var user = new User(obj);
+            user.exercise = [];
+            var exercises = dt(obj.exercise, request.query.from, request.query.to, request.query.limit);
+            exercises.forEach(element => { user.exercise.push(element); });
+            response.json(user);
+        });
 };
+
+function dt(ex, from, to, lim) {
+    var exercises = [];
+    var valid = false;
+    for (var i = 0; i < ex.length; i++) {
+        if (from != undefined && to != undefined) {
+            valid = true;
+            if (ex[i].date > new Date(from) && ex[i].date < new Date(to))
+                exercises.push(ex[i]);
+        } else if (from != undefined && to == undefined) {
+            valid = true;
+            if (new Date(ex[i].date) > new Date(from)) 
+               exercises.push(ex[i]);
+        } else if (to != undefined && from == undefined) {
+            valid = true;
+            if (ex[i].date < new Date(to))
+                exercises.push(ex[i]);
+        }
+    }
+    return exercises.length != 0 || valid ? limit(exercises, lim) : limit(ex, lim);
+}
+
+function limit(siftedWithDate, lim) {
+    if (lim > 0) {
+        return siftedWithDate.slice(0, lim);
+    }
+    return siftedWithDate;
+}
 
 module.exports = {
     users,
     createUser,
     createExercise,
-    drawFields
+    drawFields,
+    findUser
 };
